ee637c3ad0ec1e4e5fb1a048614c3f43c2994429
Ethan Allen-Harris <Ethan Allen-Harris>
2020-11-23 11:40:41 +0000

    added fully functional navigation to every page

--- server.py
+++ server.py
@@ -50,6 +50,21 @@ def checkCredentials(uName, pw):
 def index():
     return render_template('index.html')
 
+@app.route("/Users")
+def users():
+    return render_template('users.html')
+
+@app.route("/Products")
+def Products():
+    return render_template('products.html')
+
+@app.route("/Customers")
+def Customers():
+    return render_template('customers.html')
+
+@app.route("/Suppliers")
+def Suppliers():
+    return render_template('suppliers.html')
 
 
 
