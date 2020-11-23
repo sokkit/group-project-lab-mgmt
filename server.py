f2547b7d766b30ca956bd34ac88fa61207b81278
Sebastian Barnard <barnards3@cardiff.ac.uk>
2020-11-23 12:16:26 +0000

    Added hashing to main server for security

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 from flask import Flask, redirect, request, render_template, make_response, escape, session
 import sqlite3
 
@@ -24,7 +25,7 @@ app.secret_key = 'bqxe6dx12x7fxa9xd5xdcycxx04xdf4xb6chxca@;Pxdax02xcf' #using os
 def login():
     if request.method=='POST':
         uName = request.form.get('username', default="Error")
-        pw = request.form.get('password', default="Error")
+        pw = bcrypt.hashpw(request.form.get('password', default="Error").encode(),b'$2b$12$5nU0TVBvc2ZD2mLE6PztrO')
         if checkCredentials(uName, pw):
             print("checking login details")
             session['username'] = request.form['username']
@@ -44,25 +45,11 @@ def login():
         return render_template('Login.html', msg='', username = username)
 
 def checkCredentials(uName, pw):
-    return pw == 'password'
+    return pw == b"$2b$12$5nU0TVBvc2ZD2mLE6PztrOcdB.SwZnfS5Ff7PK3rQYK.gjJtu967K"
 
 @app.route("/Index")
 def index():
     return render_template('index.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
