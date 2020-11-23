2a25ed8f16ff8ba95a1242648ff69c4755661b32
Sebastian Barnard <barnards3@cardiff.ac.uk>
2020-11-23 14:08:58 +0000

    Added some changes to the script

--- hasher.py
+++ hasher.py
@@ -2,7 +2,11 @@ import bcrypt
 
 print("Added this as it may come in handy in the future (e.g. Changing password)")
 password = input("Enter the password you would like to encrypt: ")
-
-password_encrypted = bcrypt.hashpw(password.encode(),b'$2b$12$5nU0TVBvc2ZD2mLE6PztrO')
-#Uses same salt as main password, use bcrypt.gensalt() for a random one
+user_input = input("\n1: Static salt (Same as admin password) \n2: Random salt\n")
+if user_input == "1":
+    password_encrypted = bcrypt.hashpw(password.encode(),b'$2b$12$5nU0TVBvc2ZD2mLE6PztrO')
+elif user_input == "2":
+    password_encrypted = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+else:
+    pass
 print(password_encrypted)
